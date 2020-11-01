3499a49cfbd7e7f80b6f7e4788a0525a1929e263
Nícolas Telles <nico-telles@hotmail.com>
2020-10-31 21:52:11 -0300

    Média 2 - URI 1006.c
    Média 2 - URI 1006.c

---
+++ Média 2 - URI 1006.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+ double a,b,c;
+ double media;
+ scanf("%lf", &a);
+ a = a *2;
+ scanf("%lf", &b);
+ b = b*3;
+ scanf("%lf", &c);
+ c = c*5;
+ media = (a+b+c)/10;
+ printf("MEDIA = %.1lf\n",media);
+    return 0;
+}
