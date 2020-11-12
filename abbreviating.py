14f57421f5f20f09c9c14f861975c710ccb6cea9
Nícolas Telles <nico-telles@hotmail.com>
2020-11-12 18:08:14 -0300

    Abbreviating Names- Maratona InterFatecs 2020 EX A
    Yves is a receptionist at an art gallery and is organizing the opening of a new collection by a famous painter
    in the city. To save space on the pages and speed up the entry of each guest, he wants to put only an
    abbreviation of the full names of the guests in the entry list. For each guest, the first and last name must be
    inserted in full and the others in short form. Help Yves to create a program to accomplish this task.
    Input
    The entry contains several test cases, each case is a guest’s name, containing from 2 to 100 characters,
    without special characters. The entry ends with EOF.
    Output
    The program must print the guest list so that it is ordered by the names with the middle names of each guest
    abbreviated.
    Example of Input 1
    Alda Gusmao Antonia Naves
    Ismael Novais Silveira Da Silva De Melo Santos
    Prince Uria Noite
    Ismael Castelo Gorjao
    Jeferson Vargas Capistrano
    Layra Bogado
    Kyara Ramalho Cavaco
    Telmo Lagos Ourique
    Ariele Lousa
    Nicolae Chaves Fitas
    Tania Nascimento Montenegro
    Aayush Areosa Sintra
    Gastao Saraiva Guilheiro
    Ariane
    Lina Madureira Saloio
    Maratona de Programação InterFatecs 2020 - Problem A: Abbreviating Names 3
    Example of Output 1
    Aayush A. Sintra
    Alda G. A. Naves
    Ariane
    Ariele Lousa
    Gastao S. Guilheiro
    Ismael C. Gorjao
    Ismael N. S. D. S. D. M. Santos
    Jeferson V. Capistrano
    Kyara R. Cavaco
    Layra Bogado
    Lina M. Saloio
    Nicolae C. Fitas
    Prince U. Noite
    Tania N. Montenegro
    Telmo L. Ourique

---
+++ abbreviating.py
@@ -0,0 +1,20 @@
+lista_nova = []
+while (True):
+        nome_completo = input()
+        if (nome_completo == ""):
+            for i in lista_nova:
+                print(i)
+            break
+        else:
+            lista_nome = []
+            lista_nome = nome_completo.split()
+            a = len(lista_nome)
+            string = " "
+            for name in lista_nome:
+                if (name != lista_nome[0] and name != lista_nome[a-1]):
+                     string = string + name[0] + '. ' 
+            lista_nova.append(lista_nome[0]+ string + lista_nome[a-1])
+            lista_nova.sort()
+        
+             
+
