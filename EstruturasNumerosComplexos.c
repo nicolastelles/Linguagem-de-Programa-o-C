dafe2cccd226e6083153005de95105c513c5eb4f
Nícolas Telles <nico-telles@hotmail.com>
2020-12-01 11:39:20 -0300

    EstruturasNumerosComplexos.c
    Avaliação de Linguagem de Programação, 2
    o ADS- B-Matutino
    (1)(1,0) Crie uma estrutura que permita manipular números complexos!
    Considere que nesta estrutura complexo teremos real(int) e imag(int).
    Crie uma função que some duas estruturas complexo e retorne o resultado delas.
    complexo soma(complexo A, complexo B) a + bi + c + di = (a + c) + (b + d)i
    Crie uma função que multiplique dois números complexos
    complexo multi(complexo A, complexo B) (a + bi) ∗ (c + di) = (a ∗ c − b ∗ d) + (b ∗ c + a ∗ d)i
    Crie uma função que imprima um complexo na forma a+bi.
    Crie a main() para testar as funções!
    Siga o protótipo das funções! o não cumprimento levará a não conclusão da tarefa!
    1

---
+++ EstruturasNumerosComplexos.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+
+struct complexo {
+  int real;
+  int imag;
+};
+
+//função soma
+struct complexo soma(struct complexo A, struct complexo B){
+  struct complexo resposta ;
+  resposta.real = A.real + B.real;
+  resposta.imag = A.imag + B.imag;
+  return resposta;
+}
+
+//função multiplicação
+struct complexo multi(struct complexo A, struct complexo B){
+  struct complexo resposta ;
+  resposta.real = ((A.real * B.real)-(A.imag * B.imag));
+  resposta.imag = ((A.imag * B.real) + (A.real * B.imag));
+ return resposta;
+
+}
+
+int main(void) {
+
+struct complexo A;
+struct complexo B;
+struct complexo resposta;
+
+printf("Digite o número complexo A: ");
+scanf ("%d %d", &A.real, &A.imag);
+printf("Digite o número complexo B: ");
+scanf ("%d %d", &B.real, &B.imag);
+resposta = soma (A,B);
+//resposta = multi (A,B);
+if (resposta.imag > 0 )
+  printf("%d + %di",resposta.real, resposta.imag);
+else
+  printf("%d %di",resposta.real, resposta.imag);
+  return 0;
+}
